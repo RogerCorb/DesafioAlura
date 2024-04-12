d86b3470c84f897c41ee130ddc6a504fc42fa7a5
RogerCorb <rogeriocgprest@gmail.com>
2024-04-11 22:58:51 -0300

    desafio de conta bancaria exercicio alura

---
+++ src/ContaBancaria.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class ContaBancaria {
+    public static void main(String[] args) {
+        String nome = "Rogerio corbo";
+        String tipoConta = "Conta corrente";
+        double saldoConta = 1599.99;
+        int option = 0 ;
+
+        cabecalho(nome,tipoConta,saldoConta);
+
+        Scanner escolha = new Scanner(System.in);
+        while (option != 4 ){
+            menu();
+            System.out.println("Digite sua opção .: ");
+            option = escolha.nextInt();
+            if (option == 1) {
+                cabecalho(nome,tipoConta,saldoConta);
+            } else if (option == 2) {
+                saldoConta = retirada(saldoConta);
+                cabecalho(nome,tipoConta,saldoConta);
+            } else if (option == 3) {
+                saldoConta = deposito(saldoConta);
+                cabecalho(nome,tipoConta,saldoConta);
+            } else if (option > 4 || option < 0 ) {
+                System.out.println("Opção inválida !");
+            }
+        }
+    }
+    public static double retirada(double valor) {
+        Scanner valorTransferir = new Scanner(System.in);
+        System.out.println("Digite o valor a transferir : ");
+        double valorRetirada = valorTransferir.nextDouble();
+        if(valorRetirada > valor) {
+            System.out.println("Valor a transferir é maior que saldo em conta !");
+        } else {
+            valor -= valorRetirada;
+            System.out.println("Seu saldo atual é de :" + valor);
+        }
+        return valor;
+    }
+    public static double deposito(double valor) {
+        Scanner valorReceber = new Scanner(System.in);
+        System.out.println("Digite o valor a Receber : ");
+        double valorEntrada = valorReceber.nextDouble();
+        valor += valorEntrada;
+        System.out.println("Seu saldo atual é de :" + valor);
+        return valor;
+    }
+
+    public static void cabecalho(String paramum,String paramdois,Double valor) {
+        System.out.println("***********************************");
+        System.out.println("Nome Cliente : " + paramum );
+        System.out.println("Tipo de conta : " + paramdois);
+        System.out.println("Saldo Atual : " +valor);
+        System.out.println("***********************************");
+    }
+
+    public static void menu() {
+        String menu = """
+                **** escolha a sua opção **** 
+                
+                1 - consultar saldo 
+                2 - transferir valor
+                3 - receber valor
+                4 - sair
+                """;
+        System.out.println(menu);
+    }
+}
