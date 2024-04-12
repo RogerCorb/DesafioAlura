d61f2b480833855943a16ecd4edd1feec0622810
RogerCorb <rogeriocgprest@gmail.com>
2024-04-12 09:31:29 -0300

    refatorando criado metodos novos

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -5,56 +5,59 @@ public class ContaBancaria {
         String nome = "Rogerio corbo";
         String tipoConta = "Conta corrente";
         double saldoConta = 1599.99;
-        int option = 0 ;
+        int option = 0;
 
-        cabecalho(nome,tipoConta,saldoConta);
+        cabecalho(nome, tipoConta, saldoConta);
 
         Scanner entrada = new Scanner(System.in);
-        while (option != 4 ){
+        while (option != 4) {
             menu();
 
             System.out.println("Digite sua opção .: ");
             option = entrada.nextInt();
             if (option == 1) {
-                cabecalho(nome,tipoConta,saldoConta);
+                cabecalho(nome, tipoConta, saldoConta);
             } else if (option == 2) {
-                saldoConta = retirada(saldoConta,entrada);
-                cabecalho(nome,tipoConta,saldoConta);
+                saldoConta = movimentaValores(saldoConta, entrada, option);
+                cabecalho(nome, tipoConta, saldoConta);
             } else if (option == 3) {
-                saldoConta = deposito(saldoConta,entrada);
-                cabecalho(nome,tipoConta,saldoConta);
-            } else if (option != 4 ) {
+                saldoConta = movimentaValores(saldoConta, entrada, option);
+                cabecalho(nome, tipoConta, saldoConta);
+            } else if (option != 4) {
                 System.out.println("Opção inválida !");
             }
         }
     }
-    public static double retirada(double valor, Scanner entrada) {
 
-        System.out.println("Qual o valor que deseja transferir : ");
-        double valorRetirada = entrada.nextDouble();
-        if(valorRetirada > valor) {
-            System.out.println("Não ha saldo para realizar a transferência !");
-        } else {
-            valor -= valorRetirada;
-            System.out.println(String.format("Novo Saldo : R$ %.2f ",valor));
+    public static double movimentaValores(double valor, Scanner entrada, int opcao) {
+
+
+        if (opcao == 2) {
+            System.out.println("Qual o valor que deseja transferir : ");
+            double valorRetirada = entrada.nextDouble();
+            if (valorRetirada > valor) {
+                System.out.println("Não ha saldo para realizar a transferência !");
+            } else {
+                valor -= valorRetirada;
+                imprimeSaldo("Novo Saldo", valor);
+            }
+        } else if (opcao == 3) {
+            System.out.println("Digite o valor a Receber : ");
+            double valorEntrada = entrada.nextDouble();
+            valor += valorEntrada;
+            imprimeSaldo("Novo Saldo", valor);
         }
         return valor;
     }
-    public static double deposito(double valor,Scanner entrada) {
 
-        System.out.println("Digite o valor a Receber : ");
-        double valorEntrada = entrada.nextDouble();
-        valor += valorEntrada;
-        System.out.println(String.format("Novo Saldo : R$ %.2f ",valor));
-        return valor;
-    }
-    public static void cabecalho(String paramum,String paramdois,Double valor) {
+    public static void cabecalho(String paramum, String paramdois, Double valor) {
         System.out.println("***********************************");
-        System.out.println("Nome Cliente : " + paramum );
+        System.out.println("Nome Cliente : " + paramum);
         System.out.println("Tipo de conta : " + paramdois);
-        System.out.println(String.format("Saldo Atual : R$ %.2f ",valor));
+        imprimeSaldo("Saldo Atual", valor);
         System.out.println("***********************************");
     }
+
     public static void menu() {
         String menu = """
                 **** escolha a sua opção ****
@@ -64,5 +67,9 @@ public class ContaBancaria {
                 4 - sair
                 """;
         System.out.println(menu);
+    }
+
+    public static void imprimeSaldo(String mensagem, double valor) {
+        System.out.println(String.format(mensagem + ": R$ %.2f ", valor));
     }
 }
